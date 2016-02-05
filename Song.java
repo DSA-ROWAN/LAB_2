c2dc9023f259ab33c6f1709c288d559731248304
Gaudio-Joe <gaudioj7@EducLab211314.rowanads.rowan.edu>
2016-02-04 20:10:22 -0500

    Added Song and Unit

---
+++ Song.java
@@ -0,0 +1,45 @@
+package lab_2;
+
+public class Song {
+
+	String name, artist;
+	float playtime;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	private String getPlaytimeString(){
+		return "getPlaytimeString needs to be changed";
+	}
+	public double getPlaytime() {
+		return playtime;
+	}
+	public void setPlaytime(float playtime) {
+		this.playtime = playtime;
+	}
+	public Song(String nameIn, String artistIn, float playtimeIn){
+		setName(nameIn);
+		setArtist(artistIn);
+		setPlaytime(playtimeIn);
+	}
+	public String toString(){
+		return getName()+", Artist: "+getArtist()+" , playtime: "+getPlaytimeString();
+	}
+	public boolean equals(Song s){
+		boolean equals;
+		equals=this.artist.equals(s.getArtist());
+		equals&=this.name.equals(s.getName());
+		equals&=this.playtime==(s.getPlaytime());
+		return equals;
+		
+	}
+}
---
+++ UnitTest.java
@@ -0,0 +1,8 @@
+package lab_2;
+
+public class UnitTest {
+
+	public static void main(String[]args){
+		
+	}
+}
