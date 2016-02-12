b5da4c2899e8767003870a3d5c1c2cb274fb5940
gaudioj7 <gaudioj7@EducLab211314.rowanads.rowan.edu>
2016-02-11 19:53:00 -0500

    Change to Song->getPlayTimeString()->A method that should work(Untested) Change to MyArrayList. Adding override methods

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,7 +1,112 @@
-//public class MyArrayList extends List {
+public class MyArrayList extends List {
+	Song[]array;
+	public MyArrayList() {
+		array=new Song[10];
+	}
+	public MyArrayList(int size){
+		array=new Song[size];
+	}
 
-//	public MyArrayList() {
-		// TODO Auto-generated constructor stub
-	//}
+	@Override
+	public void add(int index, Object o) {
+		// TODO Auto-generated method stub
+		
+	}
 
-//}
+	@Override
+	public void add(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeAt(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Object o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set(int index, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void shift(int pos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Node getNode(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Node getNodeAt(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Node _add(int index, Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Node _add(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
